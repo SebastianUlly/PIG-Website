a225819995ddeafedd8d0d77a2bad3c0e6751b6f
SebastianUlly <s.ully@bocom.at>
2025-12-18 17:07:41 +0100

    upload try

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = 'https://grzfiyzkylmsgyjpmkav.supabase.co';
+const supabasePublishableKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyemZpeXpreWxtc2d5anBta2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMTM5NDEsImV4cCI6MjA3OTU4OTk0MX0.VpadddB4rSgXcwPMa5QAgTZvpWMmqTimCzAUrlaRPVw';
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
